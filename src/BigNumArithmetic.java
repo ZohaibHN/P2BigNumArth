462573ddfea86d56dc03d8153a7a093bfd4fa866
abroskie <adam@broskie.com>
2022-10-15 21:05:30 -0400

    Added code to remove leading whitespace & blank lines from input file

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -1,3 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
 public class BigNumArithmetic {
 
+    public static void main(String[] args) {
+        Scanner file;
+        //PrintWriter pw = new PrintWriter("output.txt");
+        try {
+                file = new Scanner(new File("BignumInput-1.txt"));
+                while (file.hasNext()) {
+                    String line = file.nextLine();
+                    line = line.trim();
+
+                    while (line.equals("")) {
+                        line = file.nextLine();
+                        line = line.trim();
+                    }
+                    System.out.println(line);
+                }
+
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
 }

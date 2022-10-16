fbe3dc42dd73b31b3d3c6c5eef5cc982c1a3249b
abroskie <adam@broskie.com>
2022-10-16 16:17:51 -0400

    Output formatting

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class BigNumArithmetic {
     static LStack stack = new LStack();
     static LList digitLinkList = new LList();
+
+
     public static void main(String[] args) {
         Scanner file;
         //PrintWriter pw = new PrintWriter("output.txt");
@@ -11,26 +13,23 @@ public class BigNumArithmetic {
                 while (file.hasNext()) {
                     String line = file.nextLine();
                     line = line.trim();
-
                     while (line.equals("")) {
                         line = file.nextLine();
                         line = line.trim();
                     }
                     line = line.replaceAll("\\s+", " "); //gets rid of excess whitespace
-                    Scanner word = new Scanner(line);
+                    Scanner singleLine = new Scanner(line);   //scanner to go through the line's elements
 
-                    while (word.hasNext()) {
-                        String nextWord = word.next();
-                        nextWord = nextWord.replaceAll("\\W", "");  //detects special characters
-                        nextWord = nextWord.replaceFirst("^0+(?!$)", ""); //trims zeroes
-                        stack.push(nextWord); //pushes number to stack
+                    while (singleLine.hasNext()) {    //loop to go through the line's characters
+                        String wholeLine = singleLine.nextLine();
 
-                        if (stack.topValue() == null) { //detects all "null" elements in stack and removes them
-                            stack.pop();
-                        } else
-                            System.out.print(stack.pop() + " "); //test to print out format and to see if zeroes are trimmed
+                        wholeLine = wholeLine.replaceAll("\\W", " ");  //detects & removes special characters
+                        wholeLine = wholeLine.replaceFirst("^0+(?!$)", ""); //trims zeroes
+                        stack.push(wholeLine); //pushes number to stack
+
+                        System.out.print(stack.pop() + "\n"); //test to print out format and to see if zeroes & special characters are trimmed
                     }
-                    System.exit(-1); //To loop through once for testing
+                    //System.exit(-1); //To loop through once for testing
                 }
 
         } catch (IOException i) {

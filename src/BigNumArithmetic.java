063ef8cebfd2878e6a072c060d4eaba3a8d5125b
zohaibhn <maakzrsmart5@gmail.com>
2022-10-16 06:12:02 -0400

    Trimmed the zeroes and extra whitespaces from lines. Left comments explaining what each new line of code does

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
 public class BigNumArithmetic {
+    static LStack stack = new LStack();
 
     public static void main(String[] args) {
         Scanner file;
@@ -15,9 +16,19 @@ public class BigNumArithmetic {
                         line = file.nextLine();
                         line = line.trim();
                     }
-                    System.out.println(line);
-                }
+                    line = line.replaceAll("\\s+", " "); //gets rid of excess whitespace
+                    Scanner word = new Scanner(line);
 
+                    while (word.hasNext()) {
+                        String nextWord = word.next();
+                        if (!nextWord.equals("+") && !(nextWord.equals("*")) && !(nextWord.equals("^"))) {
+                            nextWord = nextWord.replaceFirst("^0+(?!$)", ""); //trims zeroes
+                                stack.push(nextWord); //pushes number to stack
+                        }
+                        System.out.print(nextWord + " "); //test to print out format and to see if zeroes are trimmed
+                    }
+                    System.exit(-1); //To loop through once for testing
+                }
         } catch (IOException i) {
             i.printStackTrace();
         }
